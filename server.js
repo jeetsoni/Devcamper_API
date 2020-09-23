e3684e7f54ce883183a8a152e2ac8c0e31959ba7
jeetsoni <sonijeet1837@gmail.com>
2020-09-24 02:18:44 +0530

    third commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 //Load env vars
@@ -14,6 +15,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+//Body parser
+app.use(express.json());
+
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -21,6 +25,8 @@ if (process.env.NODE_ENV === 'development') {
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
